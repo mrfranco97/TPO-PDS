52208e0540057758dba1f66a24fa4d9cd41de7a2
MrFranco <50271790+mrfranco97@users.noreply.github.com>
2024-06-29 18:23:31 -0300

    Add files via upload

---
+++ fird_your_guide/src/main/java/find_your_guide/fird_your_guide/FirdYourGuideApplication.java
@@ -0,0 +1,13 @@
+package find_your_guide.fird_your_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirdYourGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirdYourGuideApplication.class, args);
+	}
+
+}
---
+++ fird_your_guide/src/test/java/find_your_guide/fird_your_guide/FirdYourGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package find_your_guide.fird_your_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirdYourGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
